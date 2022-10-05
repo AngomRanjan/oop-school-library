145a9c26b55a79ab22f3851072a0b88704a13809
AngomRanjan <angomranjan@gmail.com>
2022-10-05 20:32:42 +0530

    Add Trimmer Decorator

--- person.rb
+++ person.rb
@@ -8,6 +8,7 @@ class Person < Nameable
   attr_accessor :name, :age
 
   def initialize(age, name = 'Unknown', parent_permission: true)
+    super()
     @id = Random.rand(1..50)
     @age = age
     @name = name
